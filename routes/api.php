f7cff2df637e4fa0a6195faa2e178430f3413d57
marwamprog-js <marwamprog.js@gmail.com>
2022-01-04 18:42:25 -0300

    Iniciando API pedidos - Modulo 21_01

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::post('/sanctum/token', 'Api\Auth\AuthClientController@auth');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::group([
+  'middleware' => ['auth:sanctum']
+], function () {
+  Route::get('/auth/me', 'Api\Auth\AuthClientController@me');
+  Route::post('/auth/logout', 'Api\Auth\AuthClientController@logout');
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+  'prefix' => 'v1',
+  'namespace' => 'Api'
+], function () {
+  Route::get('/tenants/{uuid}', 'TenantApiController@show');
+  Route::get('/tenants', 'TenantApiController@index');
+
+  Route::get('/categories/{identify}', 'CategoryApiController@show');
+  Route::get('/categories', 'CategoryApiController@categoriesByTenant');
+
+  Route::get('/tables/{identify}', 'TableApiController@show');
+  Route::get('/tables', 'TableApiController@tablesByTenant');
+
+  Route::get('/products/{identify}', 'ProductApiController@show');
+  Route::get('/products', 'ProductApiController@productsByTenant');
+
+  Route::post('/client', 'Auth\RegisterController@store');
 });
